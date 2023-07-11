6023681d6109c3c4a5571c62aac3961b4f0285a1
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-07-11 09:57:02 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -5,11 +5,11 @@ $password = "1234";
 $dbname = "hms";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$con = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
 }
 echo "Connected successfully";
 ?>
