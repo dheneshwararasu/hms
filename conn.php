4486856ffe18177fe594e49f1acb844166db618d
dheneshwararasu <42833926+dheneshwararasu@users.noreply.github.com>
2023-07-11 10:04:32 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "1234";
-$dbname = "h";
+$dbname = "hms";
 
 // Create connection
 $con = new mysqli($servername, $username, $password, $dbname);
